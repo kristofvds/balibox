7897520ed0389bd2372afbc444f2b97c57a52380
Kristof Vandersmissen <kristof.vds@gmail.com>
2016-08-14 13:05:09 +0800

    Got Barba to work (on server)

--- js/main.js
+++ js/main.js
@@ -1,14 +1,29 @@
 $(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
+
+    // Smooth scroll
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
+    // Barba
+    var Homepage = Barba.BaseView.extend({
+        namespace: 'homepage',
+        onEnter: function() {
+            $('.navbar').addClass('navbar-transparent navbar-absolute');
+        },
+        onLeave: function() {
+            $('.navbar').removeClass('navbar-transparent navbar-absolute');
+        }
+    });
+    Homepage.init();
+    Barba.Pjax.start();
 });
---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http')
+var url = require('url')
+var fs = require('fs')
+
+http.createServer(function (request, response) {
+   var requestUrl = url.parse(request.url)    
+   response.writeHead(200)
+   fs.createReadStream(requestUrl.pathname).pipe(response)
+}).listen(8080)
